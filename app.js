2cee5e3f4f53c4792b699fc9121f13537b3bed31
Vinz <abc@xyz.com>
2024-09-04 21:47:08 +0100

    html and css for the app

---
+++ app.js
@@ -0,0 +1,8 @@
+const buttonElem = document.querySelector(".btn");
+const inputElem = document.querySelector(".input");
+let inputValue = inputElem.value;
+console.log(inputElem.value);
+
+buttonElem.addEventListener("click", () => {
+
+})
