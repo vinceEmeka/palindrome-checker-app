0816b0a051bbf4a8bbd69c4f01115d7c16b1d3cf
Vinz <abc@xyz.com>
2024-09-06 23:12:06 +0100

    COMPLETED

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const buttonElem = document.querySelector(".btn");
-const inputElem = document.querySelector(".input");
-let inputValue = inputElem.value;
-console.log(inputElem.value);
+const buttonElem = document.getElementById("check-btn");
+const inputElem = document.getElementById("text-input");
+const resultElem = document.getElementById("result");
 
-buttonElem.addEventListener("click", () => {
 
-})
+buttonElem.addEventListener("click", checkPalindrome);
+
+function checkPalindrome() {
+ if (inputElem.value === "") {
+  alert("Please input a value")
+  resultElem.innerHTML = "";
+  return;
+ }
+ const str = inputElem.value.replace(/[\W_]/g, "").toLowerCase();
+ let isPalindrome = true;
+
+ for (let i = 0; i < Math.floor(str.length / 2); i++) {
+  if (str[i] !== str[str.length - 1 - i]) {
+   isPalindrome = false;
+   break;
+  }
+ }
+
+ if (isPalindrome) {
+  resultElem.innerHTML = inputElem.value.toUpperCase() + " is a palindrome!";
+ } else {
+  resultElem.innerHTML = inputElem.value.toUpperCase() + " is not a palindrome!";
+ }
+}
